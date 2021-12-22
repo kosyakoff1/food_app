a623f96016473299d7ba7a853a6d7b2d65804585
kosyakoff1 <asondor@salusoptima.com>
2021-12-23 04:53:12 +0700

    Added filter bottom sheet design

---
+++ app/src/main/java/com/kosyakoff/foodapp/ui/fragments/recipes/RecipesBottomSheet.kt
@@ -0,0 +1,21 @@
+package com.kosyakoff.foodapp.ui.fragments.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kosyakoff.foodapp.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipes_bottom_sheet, container, false)
+    }
+
+}
