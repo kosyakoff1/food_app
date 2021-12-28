a2e0864f249bd67035f2e40d4a960c826a25967a
kosyakoff1 <asondor@salusoptima.com>
2021-12-29 05:41:07 +0700

    Changed RecipesDiffUtil to be generic

--- app/src/main/java/com/kosyakoff/foodapp/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/kosyakoff/foodapp/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.kosyakoff.foodapp.util
 import androidx.recyclerview.widget.DiffUtil
 import com.kosyakoff.foodapp.models.FoodRecipe
 
-class RecipesDiffUtil(
-    private val oldList: List<FoodRecipe>,
-    private val newList: List<FoodRecipe>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ) : DiffUtil.Callback() {
     override fun getOldListSize(): Int = oldList.size
 
