e67b534e40fad5e164a104b0460d9a360fefd651
kosyakoff1 <asondor@salusoptima.com>
2021-12-30 05:22:44 +0700

    Implemented instructions fragment

--- app/src/main/java/com/kosyakoff/foodapp/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/kosyakoff/foodapp/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,19 +1,32 @@
 package com.kosyakoff.foodapp.ui.fragments.instructions
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kosyakoff.foodapp.R
+import com.kosyakoff.foodapp.databinding.FragmentInstructionsBinding
+import com.kosyakoff.foodapp.models.FoodRecipe
+import com.kosyakoff.foodapp.ui.DetailsActivity
 
-class InstructionsFragment : Fragment() {
+class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    private val binding: FragmentInstructionsBinding by viewBinding(FragmentInstructionsBinding::bind)
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recipe = arguments?.getParcelable<FoodRecipe>(DetailsActivity.BUNDLE_KEY)!!
+
+        with(binding) {
+            instructionsWebView.webViewClient = object : WebViewClient() {}
+            instructionsWebView.settings.javaScriptEnabled = true
+            instructionsWebView.loadUrl(recipe.sourceUrl)
+        }
     }
 }
