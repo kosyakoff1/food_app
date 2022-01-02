f832d9ab872f493f06129f3fafd46dbd99ac75fc
kosyakoff1 <asondor@salusoptima.com>
2022-01-02 11:37:48 +0700

    added share function in food joke fragment

--- app/src/main/java/com/kosyakoff/foodapp/ui/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/kosyakoff/foodapp/ui/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,6 +1,10 @@
 package com.kosyakoff.foodapp.ui.fragments.foodjoke
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -21,6 +25,8 @@ class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
 
         mainViewModel.getFoodJoke(API_KEY)
         with(binding) {
@@ -48,6 +54,25 @@ class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share_food_joke_menu) {
+            val message =
+                mainViewModel.foodJokeResponse.value?.message ?: getString(R.string.str_no_joke)
+
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, message)
+                type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return true
     }
 
 }
