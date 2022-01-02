f9722b9804de49d38a94b1b5debbf82f97edccf9
kosyakoff1 <asondor@salusoptima.com>
2022-01-02 12:27:20 +0700

    added splash screen

--- app/src/main/java/com/kosyakoff/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/kosyakoff/foodapp/ui/MainActivity.kt
@@ -2,30 +2,35 @@ package com.kosyakoff.foodapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kosyakoff.foodapp.R
 import com.kosyakoff.foodapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private val binding: ActivityMainBinding by viewBinding(
-        ActivityMainBinding::bind
-    )
+class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen()
+
+        setContentView(R.layout.activity_main)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
+
         navController = navHostFragment.navController
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.recipesFragment,
@@ -34,9 +39,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             )
         )
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
