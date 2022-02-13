a5d90856e910cc69b1cb4d95866131b3ba25b00d
Alexey Sondor <>
2022-02-13 23:21:00 +0100

    Refactored OverviewFragment.kt for using single state

--- app/src/main/java/com/kosyakoff/foodapp/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/kosyakoff/foodapp/ui/fragments/overview/OverviewFragment.kt
@@ -4,38 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import coil.load
 import com.kosyakoff.foodapp.R
 import com.kosyakoff.foodapp.databinding.FragmentOverviewBinding
 import com.kosyakoff.foodapp.models.FoodRecipe
 import com.kosyakoff.foodapp.ui.DetailsActivity
+import com.kosyakoff.foodapp.viewmodels.DetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 
 
+@AndroidEntryPoint
 class OverviewFragment : Fragment(R.layout.fragment_overview) {
 
+    private val detailsViewModel: DetailsViewModel by viewModels()
     private val binding: FragmentOverviewBinding by viewBinding(FragmentOverviewBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recipe = requireArguments().getParcelable<FoodRecipe>(DetailsActivity.BUNDLE_KEY)!!
-
-        with(binding) {
-            mainImageView.load(recipe.image)
-            titleTextView.text = recipe.title
-            numberOfLikesTextView.text = recipe.aggregateLikes.toString()
-            timeTextView.text = recipe.readyInMinutes.toString()
-            val parsedString = Jsoup.parse(recipe.summary).text()
-            overviewTextView.text = parsedString
-        }
-        setCheckedItems(recipe)
+        initViewModel()
     }
 
-     private fun setCheckedItems(recipe: FoodRecipe) {
+    private fun initViewModel() {
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                detailsViewModel.uiState.collect { uiState ->
+                    with(binding) {
+                        mainImageView.load(uiState.currentRecipe.image)
+                        titleTextView.text = uiState.currentRecipe.title
+                        numberOfLikesTextView.text = uiState.currentRecipe.aggregateLikes.toString()
+                        timeTextView.text = uiState.currentRecipe.readyInMinutes.toString()
+                        val parsedString = Jsoup.parse(uiState.currentRecipe.summary).text()
+                        overviewTextView.text = parsedString
+                    }
+                    setCheckedItems(uiState.currentRecipe)
+                }
+            }
+        }
+    }
+
+    private fun setCheckedItems(recipe: FoodRecipe) {
         val checkedColor = ContextCompat.getColor(
             requireContext(),
             R.color.green
@@ -62,5 +81,4 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
             }
         }
     }
-
 }
